d9533df0304f3b0265e7116bc94f92e44a0d6434
niztg <65248634+niztg@users.noreply.github.com>
2020-08-25 16:28:34 -0400

    tweaks to alphabet_number

--- CyberTron5000/utils/lists.py
+++ CyberTron5000/utils/lists.py
@@ -5,6 +5,7 @@ For JSON lists.
 """
 
 import discord
+from string import ascii_uppercase
 
 STAT_NAMES = {
     "speed": "SPEED",
@@ -17,33 +18,8 @@ STAT_NAMES = {
 
 }
 
-ALPHABET_NUMBER = {"A": 1,
-                   "B": 2,
-                   "C": 3,
-                   "D": 4,
-                   "E": 5,
-                   "F": 6,
-                   "G": 7,
-                   "H": 8,
-                   "I": 9,
-                   "J": 10,
-                   "K": 11,
-                   "L": 12,
-                   "M": 13,
-                   "N": 14,
-                   "O": 15,
-                   "P": 16,
-                   "Q": 17,
-                   "R": 18,
-                   "S": 19,
-                   "T": 20,
-                   "U": 21,
-                   "V": 22,
-                   "W": 23,
-                   "X": 24,
-                   "Y": 25,
-                   "Z": 26
-                   }
+ALPHABET_NUMBER = {letter: number for (letter, number) in zip(ascii_uppercase, list(range(1, 27)))}
+
 NUMBER_ALPHABET = {value: key for (key, value) in ALPHABET_NUMBER.items()}
 
 TYPES = {
